ee59131971357346be2ee4bbaad994ff8ccdc884
Sidharth-006 <sidharthsingh3191@gmail.com>
2026-03-23 12:14:55 +0530

    updated project

--- src/features/WishlistSlice.js
+++ src/features/WishlistSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = { items: [] };
 
 const wishlistSlice = createSlice({
-  name: "wishlist",
+  name: "Wishlist",
   initialState,
   reducers: {
     addToWishlist: (state, action) => {
